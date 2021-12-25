f20333a4c4213ad9a63514d8bc1db7f6ae29b300
Abhijeet Gavali <mr.abhijeetgavali@gmail.com>
2021-12-25 10:02:14 +0530

    port for backend change

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
 });
 
 // defining port number
-const PORT = process.env.NODE_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 // listing on port
 app.listen(PORT, () => {
