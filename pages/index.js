3ac44358e02bddeb0465ad4ea0a7b0205906a1dc
Abhijeet Gavali <mr.abhijeetgavali@gmail.com>
2021-12-25 11:04:12 +0530

    cha

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  console.log("IP = ", process.env.IP);
+  console.log("process.env");
   return (
     <div className={styles.container}>
       HELLO WORLD FOR MONGODB PROJECT TO SUBMIT ON HACKTHON
