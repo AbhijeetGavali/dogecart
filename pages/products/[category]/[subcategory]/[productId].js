e90ad189e4b402d6643ae5662dd27b5eae06d9eb
Abhijeet Gavali <mr.abhijeetgavali@gmail.com>
2022-01-11 18:09:29 +0530

    Categories, subcategories, product-details: froentend complete

---
+++ pages/products/[category]/[subcategory]/[productId].js
@@ -0,0 +1,121 @@
+// importing dependancies
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+// importing components, styles defined
+import Product from "../../../../components/productDetails/product";
+import SmProduct from "../../../../components/productDetails/smProduct";
+import styles from "../../../../styles/productPage.module.css";
+
+// main functional component
+function ProductDetailPage({
+  categoryProducts,
+  subCategoryProducts,
+  productDetails,
+}) {
+  // using router for geting query from url
+  const router = useRouter();
+  const { category, subcategory, productId } = router.query;
+
+  // returning jsx element to client
+  return (
+    <div className={styles.productContainer}>
+      <div className={styles.productUrl}>
+        {/* top navigation link */}
+        <Link href="/">home</Link>/<Link href="/products">products</Link>/
+        <Link href="/products/[category]" as={`/products/${category}`}>
+          {category}
+        </Link>
+        /
+        <Link
+          href="/products/[category]/[subcategory]"
+          as={`/products/${category}/${subcategory}`}
+        >
+          {subcategory}
+        </Link>
+        /
+        <Link
+          href="/products/[category]/[subcategory]/[productId]"
+          as={`/products/${category}/${subcategory}/${productId}`}
+        >
+          {`${productDetails.title}`}
+        </Link>
+      </div>
+
+      {/* large product page sidplay  */}
+      <div className={styles.productDetailsPage}>
+        {" "}
+        {productDetails && <Product product={productDetails} />}
+      </div>
+
+      {/* displaying data to page of products awailable in subcategory in database */}
+      {subCategoryProducts.length > 0 && (
+        <div className={styles.categoryOuterContainer}>
+          <h2>simmilar products for {subcategory}</h2>
+          <div className={styles.categoryContainer}>
+            {subCategoryProducts.map((product, idx) => (
+              <SmProduct key={idx} product={product} />
+            ))}
+          </div>
+        </div>
+      )}
+
+      {/* displaying data to page of products awailable in category in database */}
+      {categoryProducts.length > 0 && (
+        <div className={styles.categoryOuterContainer}>
+          <h2>simmilar products for {category}</h2>
+          <div className={styles.categoryContainer}>
+            {categoryProducts.map((product, idx) => (
+              <SmProduct key={idx} product={product} />
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+  const IP2 = process.env.BACKEND_IP;
+  const { category, subcategory, productId } = ctx.query;
+
+  // fetching product details from category
+  var propData = {
+    categoryProducts: [],
+    subCategoryProducts: [],
+    productDetails: {},
+  };
+  try {
+    // getting products for category
+    await fetch(`${IP2}/api/product/${category}`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.data !== "not-found"
+          ? (propData = { ...propData, categoryProducts: [...data.data] })
+          : propData;
+      });
+
+    // getting products for sub category
+    await fetch(`${IP2}/api/product/${category}/${subcategory}`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.data !== "not-found"
+          ? (propData = { ...propData, subCategoryProducts: [...data.data] })
+          : propData;
+      });
+
+    // getting product details
+    await fetch(`${IP2}/api/product/${category}/${subcategory}/${productId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.data !== "not-found"
+          ? (propData = { ...propData, productDetails: data.data })
+          : propData;
+      });
+    return { props: propData };
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export default ProductDetailPage;
---
+++ pages/products/[category]/[subcategory]/index.js
@@ -0,0 +1,161 @@
+// importing dependencies
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+// importing components, styles defined
+import SmProduct from "../../../../components/productDetails/smProduct";
+import styles from "../../../../styles/subCatrgoryProductPage.module.css";
+
+// main functional component
+function Subcategory({ categoryProducts, subCategoryProducts }) {
+  // define IP of backend to get data
+  const IP = "http://localhost:5000";
+
+  // using router for geting query from url
+  const router = useRouter();
+  const { category, subcategory } = router.query;
+
+  const [categoryProduct, setcategoryProduct] = useState(categoryProducts);
+  const [subCategoryProduct, setSubCategoryProduct] =
+    useState(subCategoryProducts);
+  const [count, setCount] = useState({ category: 10, subcategory: 30 });
+
+  // returning jsx element to client
+  return (
+    <div className={styles.productContainer}>
+      <div className={styles.productNavContainer}>
+        <div className={styles.productUrl}>
+          {/* top navigation link */}
+          <Link href="/">home</Link>/<Link href="/products">products</Link>/
+          <Link as={`/products/${category}`} href="/products/[category]">
+            {category}
+          </Link>
+          /
+          <Link
+            href="/products/[category]/[subcategory]"
+            as={`/products/${category}/${subcategory}`}
+          >
+            {subcategory}
+          </Link>
+        </div>
+
+        {/* normal select dropdown to the page  */}
+        <select name="sort" id="sort" className={styles.sortOption}>
+          <option default value="select">
+            Select Filter
+          </option>
+          <option value="highRated">High Rated</option>
+          <option value="Latest">New Posted</option>
+        </select>
+      </div>
+
+      {/* displaying data to page of products awailable in subcategory in database */}
+      {subCategoryProduct.length > 0 && (
+        <div className={styles.categoryOuterContainer}>
+          <h2>
+            products found for {category} {subcategory}
+          </h2>
+          <div className={styles.categoryContainer}>
+            {subCategoryProduct.map((product, idx) => (
+              <SmProduct key={idx} product={product} />
+            ))}
+          </div>
+
+          {/* for loading more result  */}
+          <button
+            className={styles.button + " bg_gradient"}
+            onClick={async () => {
+              await fetch(`${IP}/api/product/${category}`, {
+                count: count.subcategory,
+              })
+                .then((response) => response.json())
+                .then((data) => {
+                  if (data.data !== "not-found") {
+                    setSubCategoryProduct([...data.data]);
+                    setCount({ ...count, subcategory: count.subcategory + 10 });
+                  } else {
+                    setSubCategoryProduct([]);
+                  }
+                });
+            }}
+          >
+            Load More
+          </button>
+        </div>
+      )}
+
+      {/* displaying data to page of products awailable in category in database */}
+      {categoryProduct.length > 0 && (
+        <div className={styles.categoryOuterContainer}>
+          <h2>simmilar products for {category}</h2>
+          <div className={styles.categoryContainer}>
+            {categoryProduct.map((product, idx) => (
+              <SmProduct key={idx} product={product} />
+            ))}
+          </div>
+
+          {/* for loading more result  */}
+          <button
+            className={styles.button + " bg_gradient"}
+            onClick={async () => {
+              await fetch(`${IP}/api/product/${category}`, {
+                count: count.category,
+              })
+                .then((response) => response.json())
+                .then((data) => {
+                  if (data.data !== "not-found") {
+                    setSubCategoryProduct([...data.data]);
+                    setCount({ ...count, subcategory: count.subcategory + 10 });
+                  } else {
+                    setSubCategoryProduct([]);
+                  }
+                });
+            }}
+          >
+            Load More
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+// generating props for loading data of the page on server
+export async function getServerSideProps(ctx) {
+  const IP2 = process.env.BACKEND_IP;
+  const { category, subcategory } = ctx.query;
+
+  // define props object to usee
+  var propData = {
+    categoryProducts: [],
+    subCategoryProducts: [],
+  };
+
+  try {
+    // fetching product details from category
+    await fetch(`${IP2}/api/product/${category}`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.data !== "not-found"
+          ? (propData = { ...propData, categoryProducts: [...data.data] })
+          : propData;
+      });
+
+    // fetching product details from subcategory
+    await fetch(`${IP2}/api/product/${category}/${subcategory}`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.data !== "not-found"
+          ? (propData = { ...propData, subCategoryProducts: [...data.data] })
+          : propData;
+      });
+    return { props: propData };
+  } catch (e) {
+    // error occured while fetching data
+    console.log("error from category page : ", e);
+    return { props: propData };
+  }
+}
+
+export default Subcategory;
