709a48b6d86159dacb982233cb0deada22699b10
Abhijeet Gavali <mr.abhijeetgavali@gmail.com>
2022-01-13 00:30:36 +0530

    product api update

---
+++ pages/user/orders/[id].js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function OrderIdDetails() {
+  return (
+    <div>
+      Details with order id full invoice details with tracking map it is under
+      auth conditions
+    </div>
+  );
+}
