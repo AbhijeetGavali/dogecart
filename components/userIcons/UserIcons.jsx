11bacc020f56f7688677b1e5da23be82a9fb29fa
Abhijeet Gavali <mr.abhijeetgavali@gmail.com>
2022-01-13 00:11:59 +0530

    static cart UI design

--- components/userIcons/UserIcons.jsx
+++ components/userIcons/UserIcons.jsx
@@ -1,17 +1,43 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Cart from "../cart/Cart";
+import MiniDashboard from "../userMiniDashboard/MiniDashboard";
 import styles from "./userIcons.module.css";
 
 export default function UserIcons() {
+  const [toggleCart, setToggleCart] = useState(false);
+  const [userHome, setUserHome] = useState(false);
+  const router = useRouter();
   return (
-    <div className={`${styles.userIcons}`}>
-      <div className={`${styles.bottomIcons} bg_gradient`}>
-        <div className={`${styles.userIcon}`}></div>
+    <>
+      <div className={`${styles.userIcons}`}>
+        <div className={`${styles.bottomIcons} bg_gradient`}>
+          <div
+            onClick={() => {
+              router.push("/store/create-new");
+            }}
+            className={`${styles.storeIcon}`}
+          ></div>
+        </div>
+        <div className={`${styles.bottomIcons} bg_gradient`}>
+          <div
+            onClick={() => {
+              setToggleCart(!toggleCart);
+            }}
+            className={`${styles.cartIcon}`}
+          ></div>
+        </div>
+        <div className={`${styles.bottomIcons} bg_gradient`}>
+          <div
+            onClick={() => {
+              setUserHome(!userHome);
+            }}
+            className={`${styles.userIcon}`}
+          ></div>
+        </div>
       </div>
-      <div className={`${styles.bottomIcons} bg_gradient`}>
-        <div className={`${styles.storeIcon}`}></div>
-      </div>
-      <div className={`${styles.bottomIcons} bg_gradient`}>
-        <div className={`${styles.cartIcon}`}></div>
-      </div>
-    </div>
+      {!toggleCart && <Cart />}
+      {!userHome && <MiniDashboard />}
+    </>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["assets.myntassets.com"],
+    domains: [
+      "assets.myntassets.com",
+      "images.unsplash.com",
+      "i.pinimg.com",
+      "cdn.shopify.com",
+      "rukminim1.flixcart.com",
+      // temp
+      "media.istockphoto.com",
+    ],
   },
 };
